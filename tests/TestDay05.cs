a9fe0dde27e7a58b9b79667abe8fef8c35dd5924
Edward Toroshchyn <dev@hades.name>
2024-08-01 23:44:23 +0200

    solve day 5

---
+++ tests/TestDay05.cs
@@ -0,0 +1,22 @@
+namespace tests;
+
+using aoc16;
+
+public class TestDay05
+{
+    [Fact(Timeout = 30)]
+    public void TestFirstPart()
+    {
+        var solver = new Day05();
+        solver.Presolve("abc");
+        Assert.Equal("18f47a30", solver.SolveFirst());
+    }
+
+    [Fact(Timeout = 90)]
+    public void TestSecondPart()
+    {
+        var solver = new Day05();
+        solver.Presolve("abc");
+        Assert.Equal("05ace8e3", solver.SolveSecond());
+    }
+}
